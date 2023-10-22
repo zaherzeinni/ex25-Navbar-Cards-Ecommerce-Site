77d828b88f4f7464714034bc39bc67f9b0813007
Zaher Zeinni <55919678+zaherzeinni@users.noreply.github.com>
2023-10-22 23:00:13 +0000

    g

--- ex25-Navbar-Cards-Ecommerce-Site/src/Components/Navbar.jsx
+++ ex25-Navbar-Cards-Ecommerce-Site/src/Components/Navbar.jsx
@@ -2,7 +2,7 @@
 import { React } from 'react';
 import { Box } from '@chakra-ui/react';
 import {ShoppingCartOutlined,LoginOutlined} from '@ant-design/icons'
-
+//
 const Navbar = ({cart,navigateTo}) => {
 
 
